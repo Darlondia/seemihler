c6b6985a3622d5d8f04abe98fe9b7dc451ba7040
EEXimium <ataseverefeyagiz@gmail.com>
2020-09-17 00:24:47 +0300

    sa
    as

---
+++ seemihlerunityproject/Assets/scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float right;
+    public float left;
+    Vector2 vec;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey("d"))
+        {
+            rb.velocity = new Vector2(right*Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.velocity = new Vector2(-left * Time.deltaTime, 0);
+        }
+
+    }
+
+}
