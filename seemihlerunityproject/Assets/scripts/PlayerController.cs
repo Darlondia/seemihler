947ef9a95cc46abab4ebb1f90eff535399b9303c
EEXimium <ataseverefeyagiz@gmail.com>
2020-09-17 12:53:59 +0300

    ANIMASYON GUNCELLEMESI_1
    Abiler animasyonları cilaladım geriye fizikleri cilalamak kaldı

--- seemihlerunityproject/Assets/scripts/PlayerController.cs
+++ seemihlerunityproject/Assets/scripts/PlayerController.cs
@@ -6,25 +6,52 @@ public class PlayerController : MonoBehaviour
     public float right;
     public float left;
     Vector2 vec;
+    public Animator animator;
 
     void Start()
     {
         
     }
 
+
+    void Update()
+    {
+        //---------SAGA DOGRU HAREKET ANIMASYONU----------------------
+        if (Input.GetKeyDown("d"))
+        {
+            animator.SetFloat("SpeedSag", right);
+        }
+        if (Input.GetKeyUp("d"))
+        {
+            animator.SetFloat("SpeedSag", 0);
+        }
+
+        //-------------SOLA DOĞRU HAREKET ANIMASYONU------------------
+        if (Input.GetKeyDown("a"))
+        {
+            animator.SetFloat("SpeedSol", left);
+        }
+        if (Input.GetKeyUp("a"))
+        {
+            animator.SetFloat("SpeedSol", 0);
+        }
+
+    }
+
     void FixedUpdate()
     {
 
+        //----------SAGA HAREKET------------
         if (Input.GetKey("d"))
         {
             rb.velocity = new Vector2(right*Time.deltaTime, 0);
         }
 
+        //----------SOLA HAREKET------------
         if (Input.GetKey("a"))
         {
             rb.velocity = new Vector2(-left * Time.deltaTime, 0);
         }
 
     }
-
 }
