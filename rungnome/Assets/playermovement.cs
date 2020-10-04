0996c4ecab0d327c105cca31b5a6eb67bf89d616
Darlondia <caner.gezer@hotmail.com>
2020-10-04 23:20:16 +0300

    gnomegame
    gnomegame

---
+++ rungnome/Assets/playermovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+   
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+
+
+
+        
+    }
+}
