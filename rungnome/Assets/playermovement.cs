762565ebcb4e7328c1e8038e7e5020f0b3f663d2
EEXimium <ataseverefeyagiz@gmail.com>
2020-10-04 23:42:40 +0300

    hareket scripti yeğen
    31 (annayan annadı)

--- rungnome/Assets/playermovement.cs
+++ rungnome/Assets/playermovement.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
-   
 
-    // Update is called once per frame
+    public Rigidbody2D rb;
+    public float right;
+    public float left;
+    public float jump;
+    
     void FixedUpdate()
     {
-        
 
+        //----------SAGA HAREKET------------
+        if (Input.GetKey("d"))
+        {
+            rb.velocity = new Vector2(right * Time.deltaTime, rb.velocity.y);
+        }
 
-
-        
+        //----------SOLA HAREKET------------
+        if (Input.GetKey("a"))
+        {
+            rb.velocity = new Vector2(-left * Time.deltaTime, rb.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jump;
+        }
     }
+
+
+
 }
+
