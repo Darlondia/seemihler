65323d7bd1a3d1946866aed365d785357bd6bb8f
Darlondia <caner.gezer@hotmail.com>
2020-10-05 17:52:01 +0300

    sa
    sa

--- gameproject2d/Assets/playermovement.cs
+++ gameproject2d/Assets/playermovement.cs
@@ -4,26 +4,50 @@ using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    Vector2 movement;
     public Rigidbody2D rb;
-    public float right = 250.0f;
-    public float left = 250.0f;
-    public float jump = 15.0f;
+    public float right = 250f;
+    public float left = 250f;
+    public float jump = 15f;
+    public float down = 8f;
+    public bool isGrounded = false;
+
+
+
+    private void Update()
+    {
+
+       
+        
+    }
+
 
     void FixedUpdate()
     {
-        if (Input.GetKey("d"))
-        {
-            rb.velocity = new Vector2(right * Time.deltaTime, rb.velocity.y);
-        }
 
-        //----------SOLA HAREKET------------
         if (Input.GetKey("a"))
         {
             rb.velocity = new Vector2(-left * Time.deltaTime, rb.velocity.y);
+            if (Input.GetKey("s"))
+            {
+                rb.velocity = new Vector2(-left * Time.deltaTime, -down);
+            }
+
         }
-        if (Input.GetKeyDown("w"))
+        if (Input.GetKey("d"))
+        {
+            rb.velocity = new Vector2(right * Time.deltaTime, rb.velocity.y);
+            if (Input.GetKey("s"))
+            {
+                rb.velocity = new Vector2(right * Time.deltaTime, -down);
+            }
+        }
+        if (Input.GetKeyDown("w")&& isGrounded == true)
         {
             rb.velocity = Vector2.up * jump;
         }
+       
+
     }
 }
